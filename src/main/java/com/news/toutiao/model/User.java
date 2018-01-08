97ad007f9d975ada87edb89690731871b96b3359
HuaLiLiZhang <1151874043@qq.com>
2018-01-08 18:38:22 +0800

    mybatis add

--- src/main/java/com/news/toutiao/model/User.java
+++ src/main/java/com/news/toutiao/model/User.java
@@ -4,6 +4,34 @@ package com.news.toutiao.model;
  * Created by huali on 2018/1/3.
  */
 public class User {
+
+    public User() {
+        super();
+    }
+
+    private int id;
+
+
+    private String name;
+    private String password;
+    private String salt;
+    private String headUrl;
+
+    public User(String name) {
+        this.name = name;
+        this.password="";
+        this.salt="";
+        this.headUrl="";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
@@ -12,13 +40,27 @@ public class User {
         this.name = name;
     }
 
-    private String name;
-
-    public User(String name)
-    {
-        this.name=name;
+    public String getPassword() {
+        return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public String getSalt() {
+        return salt;
+    }
 
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getHeadUrl() {
+        return headUrl;
+    }
+
+    public void setHeadUrl(String headUrl) {
+        this.headUrl = headUrl;
+    }
 }
