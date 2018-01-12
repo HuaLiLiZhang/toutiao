bbf208f86c63de720ada237301b5a674988fcc81
HuaLiLiZhang <1151874043@qq.com>
2018-01-12 11:32:32 +0800

    display one user in the web

--- src/main/java/com/news/toutiao/controller/HomeController.java
+++ src/main/java/com/news/toutiao/controller/HomeController.java
@@ -7,9 +7,9 @@ import com.news.toutiao.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -26,10 +26,10 @@ public class HomeController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(path={"/","index"},method={RequestMethod.GET,RequestMethod.POST})
-    public String index(Model model)
+
+    private List<ViewObject> getNews(int userId, int offset, int limit)
     {
-        List<News> newsList=newsService.getLatestNews(0,0,10);
+        List<News> newsList=newsService.getLatestNews(userId,offset,limit);
         List<ViewObject> vos=new ArrayList<>();
         for(News news:newsList)
         {
@@ -39,7 +39,19 @@ public class HomeController {
             vo.set("user",userService.getUser(news.getUserId()));
             vos.add(vo);
         }
-        model.addAttribute("vos",vos);
+        return vos;
+    }
+    @RequestMapping(path={"/","index"},method={RequestMethod.GET,RequestMethod.POST})
+    public String index(Model model)
+    {
+        model.addAttribute("vos",getNews(0,0,10));
+        return "home";
+    }
+
+    @RequestMapping(path = {"/user/{userId}"},method = {RequestMethod.GET,RequestMethod.POST})
+    public String userIndex(Model model, @PathVariable("userId") int userId)
+    {
+        model.addAttribute("vos",getNews(userId,0,10));
         return "home";
     }
 
