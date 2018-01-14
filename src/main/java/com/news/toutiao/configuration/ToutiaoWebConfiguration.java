68f17e16af8780340c559d9f9bf45e4cfc726bb5
HuaLiLiZhang <1151874043@qq.com>
2018-01-14 12:04:28 +0800

    add

--- src/main/java/com/news/toutiao/configuration/ToutiaoWebConfiguration.java
+++ src/main/java/com/news/toutiao/configuration/ToutiaoWebConfiguration.java
@@ -1,11 +1,24 @@
 package com.news.toutiao.configuration;
 
+import com.news.toutiao.interceptor.PassportInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by huali on 2018/1/13.
  */
+@Component
 public class ToutiaoWebConfiguration extends WebMvcConfigurerAdapter{
+    //加入拦截器
+    @Autowired
+    PassportInterceptor passportInterceptor;
 
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(passportInterceptor);  //每次注册时，会回调一遍
+        super.addInterceptors(registry);
+    }
 }
