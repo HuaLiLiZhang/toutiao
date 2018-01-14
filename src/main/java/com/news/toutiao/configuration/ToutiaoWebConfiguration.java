fdc6c0170e80501c976a2ce2ee7d839a61c5bf2d
HuaLiLiZhang <1151874043@qq.com>
2018-01-14 14:35:40 +0800

    setting login required Interceptor

--- src/main/java/com/news/toutiao/configuration/ToutiaoWebConfiguration.java
+++ src/main/java/com/news/toutiao/configuration/ToutiaoWebConfiguration.java
@@ -1,5 +1,6 @@
 package com.news.toutiao.configuration;
 
+import com.news.toutiao.interceptor.LoginRequiredInterceptor;
 import com.news.toutiao.interceptor.PassportInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,14 +12,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Component
 public class ToutiaoWebConfiguration extends WebMvcConfigurerAdapter{
-    //加入拦截器
+    //未登录跳转，加入拦截器
     @Autowired
     PassportInterceptor passportInterceptor;
+
+    @Autowired
+    LoginRequiredInterceptor loginRequiredInterceptor;
 
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(passportInterceptor);  //每次注册时，会回调一遍
+        registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/setting*");  //每次注册时，会回调一遍
+                                                                    //setting界面也设置跳转
+
         super.addInterceptors(registry);
     }
 }
---
+++ src/main/java/com/news/toutiao/interceptor/LoginRequiredInterceptor.java
@@ -0,0 +1,52 @@
+package com.news.toutiao.interceptor;
+
+import com.news.toutiao.dao.LoginTicketDAO;
+import com.news.toutiao.dao.UserDAO;
+import com.news.toutiao.model.HostHolder;
+import com.news.toutiao.model.LoginTicket;
+import com.news.toutiao.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+/**
+ * Created by huali on 2018/1/13.
+ */
+@Component
+public class LoginRequiredInterceptor implements HandlerInterceptor{
+    //2.做一个拦截器，判断登陆的拦截器
+    //
+    @Autowired
+    private HostHolder hostHolder;
+
+
+
+    @Override  //add the override at the generate
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        if (hostHolder.getUser()==null)
+        {
+            httpServletResponse.sendRedirect("/?pop=1");
+            //设置setting访问跳转
+            return false;
+        }
+        return  true;
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
