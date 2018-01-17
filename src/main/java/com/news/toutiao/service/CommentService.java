4e3fc243a3931ff87d2a89d54fa7ce193a4e1594
HuaLiLiZhang <1151874043@qq.com>
2018-01-17 16:35:29 +0800

    add MessageDAO.java

--- src/main/java/com/news/toutiao/service/CommentService.java
+++ src/main/java/com/news/toutiao/service/CommentService.java
@@ -1,7 +1,33 @@
 package com.news.toutiao.service;
 
+import com.news.toutiao.dao.CommentDAO;
+import com.news.toutiao.model.Comment;
+import org.apache.ibatis.annotations.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
  * Created by huali on 2018/1/17.
  */
+@Mapper
 public class CommentService {
+    private static final Logger logger = LoggerFactory.getLogger(CommentService.class);
+
+    @Autowired
+    CommentDAO commentDAO;
+
+    public List<Comment> getCommentsByEntity(int entityId, int entityType) {
+        return commentDAO.selectByEntity(entityId, entityType);
+    }
+
+    public int addComment(Comment comment) {
+        return commentDAO.addComment(comment);
+    }
+
+    public int getCommentCount(int entityId, int entityType) {
+        return commentDAO.getCommentCount(entityId, entityType);
+    }
 }
