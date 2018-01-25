cc98ca6f24810984fcbafa6ca0cb23a3cf598196
HuaLiLiZhang <1151874043@qq.com>
2018-01-25 20:02:39 +0800

    add commentService

---
+++ src/main/java/com/news/toutiao/service/CommentService.java
@@ -0,0 +1,34 @@
+package com.news.toutiao.service;
+
+import com.news.toutiao.dao.CommentDAO;
+import com.news.toutiao.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by huali on 2018/1/25.
+ */
+@Service
+public class CommentService {
+    @Autowired
+    private CommentDAO commentDAO;
+
+    public List<Comment> getCommentsByEntity(int entityId,int entityType)
+    {
+        return commentDAO.selectByEntity(entityId,entityType);
+    }
+
+    public int addComment(Comment comment)
+    {
+        return commentDAO.addComment(comment);
+    }
+
+    public int getCommentCount(int entityId,int entityType)
+    {
+        return commentDAO.getCommentCount(entityId,entityType);
+    }
+
+
+}
