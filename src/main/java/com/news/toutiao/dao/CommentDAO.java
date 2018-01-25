2b717776e59f24bd6b3021d63d8646929d26d64c
HuaLiLiZhang <1151874043@qq.com>
2018-01-25 16:52:34 +0800

    add commentDAO

--- src/main/java/com/news/toutiao/dao/CommentDAO.java
+++ src/main/java/com/news/toutiao/dao/CommentDAO.java
@@ -1,6 +1,7 @@
 package com.news.toutiao.dao;
 
 import com.news.toutiao.model.Comment;
+import jdk.nashorn.internal.objects.annotations.Where;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -10,22 +11,24 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 /**
- * Created by huali on 2018/1/17.
+ * Created by huali on 2018/1/25.
  */
 @Repository
 @Mapper
+
 public interface CommentDAO {
-    //String TABLE_NAME = " comment ";
-    //String INSERT_FIELDS = " user_id, content, created_date, entity_id, entity_type, status ";
-    //String SELECT_FIELDS = " id, " + INSERT_FIELDS;
-    //
-    //@Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS,
-    //        ") values (#{userId},#{content},#{createdDate},#{entityId},#{entityType},#{status})"})
-    //int addComment(Comment comment);
-    //
-    //@Select({"select ", SELECT_FIELDS, " from ", TABLE_NAME, " where entity_type=#{entityType} and entity_id=#{entityId} order by id desc "})
-    //List<Comment> selectByEntity(@Param("entityId") int entityId, @Param("entityType") int entityType);
-    //
-    //@Select({"select count(id) from ", TABLE_NAME, " where entity_type=#{entityType} and entity_id=#{entityId}"})
-    //int getCommentCount(@Param("entityId") int entityId, @Param("entityType") int entityType);
+    String TABLE_NAME="comment";
+    String INSERT_FIELDS="user_id,content,created_date,entity_id,entity_type,status";
+    String SELECT_FILEDS="id,"+INSERT_FIELDS;
+
+    @Insert({"insert into",TABLE_NAME,"(",INSERT_FIELDS,
+    ") values (#{userId},#{content},#{createdDate},#{entityId},#{entityType},#{status})"})
+    int addComment(Comment comment);
+
+
+    @Select({"select",SELECT_FILEDS,"from",TABLE_NAME,
+            "where entity_id=#{entityId} and entity_type=#{entityType} order by id desc" })
+    List<Comment> selectByEntity(@Param("entityID") int entityId,
+                                 @Param("entityType") int entityType);
+
 }
--- src/main/java/com/news/toutiao/model/EntityType.java
+++
@@ -1,8 +0,0 @@
-package com.news.toutiao.model;
-
-/**
- * Created by huali on 2018/1/17.
- */
-public class EntityType {
-    public static int ENTITY_NEWS = 1;
-}
