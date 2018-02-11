ba88314e368bd8227d95de4b346deea2a3008a30
HuaLiLiZhang <1151874043@qq.com>
2018-02-11 15:07:05 +0800

    setObject /getObject

--- src/main/java/com/news/toutiao/util/JedisAdapter.java
+++ src/main/java/com/news/toutiao/util/JedisAdapter.java
@@ -366,21 +366,22 @@ public class JedisAdapter implements InitializingBean{
         }
     }
 
+    //
+    public void setObject(String key,Object obj)
+    {
+        set(key, JSON.toJSONString(obj));
+    }
 
-    //public void setObject(String key, Object obj)
-    //{
-    //    set(key,JSON.toJSONString(obj));
-    //}
-    //public <T> T getObject(String key,Class<T> clazz)
-    //{
-    //    String value=get(key);
-    //    if(value!=null)
-    //    {
-    //        return JSON.parseObject(value, clazz);
-    //    }
-    //    return null;
-    //}
+    public <T> T getObject(String key,Class<T> clazz)
+    {
+        String value=get(key);  //取出是是一个json
+        if(value!=null)
+        {
+            return JSON.parseObject(value,clazz);   // 把JSON文本parse为JavaBean
+        }
 
+        return null;
+    }
 
 
 }
