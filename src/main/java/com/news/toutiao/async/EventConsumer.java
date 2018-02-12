a23641631183684b4651b4530c58f963c21c2191
HuaLiLiZhang <1151874043@qq.com>
2018-02-12 11:30:56 +0800

    add EventConsumer deal with the event and overrite the InitialzingBean ,ApplicationContextAware

--- src/main/java/com/news/toutiao/async/EventConsumer.java
+++ src/main/java/com/news/toutiao/async/EventConsumer.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by huali on 2018/2/11.
@@ -17,13 +21,31 @@ import org.springframework.stereotype.Service;
 public class EventConsumer  implements InitializingBean, ApplicationContextAware{
     private static final Logger logger= LoggerFactory.getLogger(EventConsumer.class);
 
+    private Map<EventType,List<EventHandler>> config= new HashMap<EventType,List<EventHandler>>();
+    private ApplicationContext applicationContext;
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        Map<String, EventHandler> beans= applicationContext.getBeansOfType(EventHandler.class);
+        if(beans!=null)
+        {
+            for(Map.Entry<String,EventHandler> entry:beans.entrySet())
+            {
+                List<EventType> eventTypes=entry.getValue().getSupportEventType();
+                for(EventType type:eventTypes)
+                {
+                    if(!config.containsKey(type))
+                    {
+                        config.put(type,new ArrayList<EventHandler>());
+                    }
+                    config.get(type).add(entry.getValue());
+                }
+            }
+        }
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        
+
     }
 }
