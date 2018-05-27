e00b6e807d1c36e9db31ef403d93da70293dc048
HuaLiLiZhang <1151874043@qq.com>
2018-05-27 16:28:14 +0800

    Spring的AOP，面向切面编程，添加日志，调用函数之前之后，都调用此方法

--- src/main/java/com/news/toutiao/aspect/LogAspect.java
+++ src/main/java/com/news/toutiao/aspect/LogAspect.java
@@ -15,21 +15,34 @@ import java.util.Date;
 /**
  * Created by huali on 2018/1/4.
  */
-@Aspect
+@Aspect  //定义一个切面。
 @Component
 public class LogAspect {
     private static final Logger logger= LoggerFactory.getLogger(LogAspect.class);
 
+    //面向切面编程，对日志进行统一格式的打印，但是又不能在每一个Controller添加，比较麻烦。
+    //所以这里使用AOP：面向切面的编程，定义一个log日志的类，统一处理，调用的函数的并打印日志
+
+    //这里在所有执行controller包下的所有类 之前和 之后 都要调用这两种方法。
     @Before("execution(* com.news.toutiao.controller.*Controller.*(..))")
-    public void beforeMethod(JoinPoint joinPoint)
+    public void beforeMethod(JoinPoint joinPoint)  //JoinPoint：表示切点。
     {
         //before and the after the logger all is be used in IndexController function.
-        //通配符：*Controller
+        //通配符：*Controller ：表示Controller前面任意字符
+        //格式为execution(* 包名(..))  /这里* ：*后面有一个空格， (..)表示包下所有方法。
         StringBuilder sb=new StringBuilder();
-        for(Object arg:joinPoint.getArgs())
+        for(Object arg:joinPoint.getArgs())  //这里是切面的参数，也就是输入到地址栏的参数。
         {
             sb.append("arg:"+arg.toString()+"|");
         }
+
+        //比如访问：http://127.0.0.1:8080/admin?key=admind
+        //参数  admind ,所以jointpoint的getArgs()是before method;arg:admind|
+        //日志结果：before method;arg:admind|
+        // after method;
+        //由于访问错误，throw new IllegalArgumentException()调用了异常捕捉函数。
+        //结果：before method;arg:java.lang.IllegalArgumentException: key error错误 !!! 请重新输入key_|
+        //after method;
 
         //logger.info("before : time:",new Date());
         logger.info("before method;"+sb.toString());
