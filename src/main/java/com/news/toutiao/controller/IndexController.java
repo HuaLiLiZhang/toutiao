446e356b9bcf2281df51db3bef71f1d224952534
HuaLiLiZhang <1151874043@qq.com>
2018-05-27 15:53:28 +0800

    add more explain

--- src/main/java/com/news/toutiao/controller/IndexController.java
+++ src/main/java/com/news/toutiao/controller/IndexController.java
@@ -33,6 +33,7 @@ public class IndexController {
     private ToutiaoService toutiaoService;
 
 
+    //初始做页面的时候，只返回一直固定的参数。之后主页面改为home.html自己的主页面，就不返回这个了。
     @RequestMapping(path={"/","/index"})  //接受的是一个URL的地址，访问首页，就用函数处理
     @ResponseBody
     public String  index(HttpSession session )
@@ -40,9 +41,30 @@ public class IndexController {
         logger.info("visit Index!");
         //add logger
         logger.trace("hello ,logger trace");
+        //在做重定向的时候，重定向向session中添加了属性SetAttribute("msg","Jump from redirect")
+        //重定向之后，会跳转到这个页面。因为重定向返回的URL就是主页。
+        //主页显示：
+        //hello nowCoder,Jump from redirect
+        //Say:this is from toutiaoservice   这里是从service层返回的消息。调用函数嘛。
         return "hello nowCoder,"+session.getAttribute("msg")
                 +"<br> Say:"+toutiaoService.say();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -84,12 +106,13 @@ public class IndexController {
 
 
 
+    //获取请求get的信息
     @RequestMapping(value={"/request"})
     @ResponseBody
-    public String request(HttpServletRequest request,
+    public String request(HttpServletRequest request/*,
                           HttpServletResponse response,
-                          HttpSession session)
-    {
+                          HttpSession session*/)
+    {  //获取http浏览器的请求信息，名字和内容，
         StringBuilder sb=new StringBuilder();
         //StringBuffer sb=new StringBuffer();
         Enumeration<String> headerNames=request.getHeaderNames();
@@ -123,7 +146,7 @@ public class IndexController {
 
 
 
-
+    //返回响应内容，自己可以在返回响应的内容里面加东西，自定义返回内容。
     @RequestMapping(value={"/response"})
     @ResponseBody
     public String response(@CookieValue(value = "nowcoderid",defaultValue = "a") String nowcoderId,
@@ -133,29 +156,45 @@ public class IndexController {
     {
         response.addCookie(new Cookie(key,value));
         response.addHeader(key,value);
-        return "NowcoderId from cookies:"+nowcoderId;
+        return "NowcoderId from cookies:"+nowcoderId+"  key:"+key+"   value:"+value;
+    //    NowcoderId from cookies:23456 key:huali value:23456   nowcoderId其实就等于value的值
+    //    通过request获取数据，或者response将更多的数据写回去。
+    //    http://127.0.0.1:8080/response?nowcoderid=9347&key=huali&value=23456
+    //    Query String Paramters:
+    //    nowcoderid: 9347
+        //key: huali
+        //value: 23456
     }
 
 
 
 
 
-    @RequestMapping("/redirect/{code}")
-    public /*RedirectView*/String  redirect(@PathVariable("code") @NumberFormat int code,
+    @RequestMapping(value = {"/redirect/{code}"})
+    public /*RedirectView*/   String  redirect(@PathVariable("code") @NumberFormat int code,
                                             HttpSession session)
     {
-        /*RedirectView red=new RedirectView("/",true);
-        if(code == 301)
-        {
-            red.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
-        }
-        return red;*/
+        //RedirectView red=new RedirectView("/",true);
+        //if(code == 301)
+        //{
+        //    red.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        //}
+        //return red;
 
-        //return "redirect:/";
         session.setAttribute("msg","Jump from redirect");
+        //这里可以添加参数，然后我们可以在首页显示出来，就是 index 首页，最前面的index函数
         return "redirect:/";
 
     }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -164,22 +203,26 @@ public class IndexController {
     @ResponseBody
     public String admin(@RequestParam(value="key",required = false)String key)
     {
+        //http://127.0.0.1:8080/admin?key=admin
+        //页面显示：hello,admin
         if("admin".equals(key))
             return "hello,admin";
 
-        throw new IllegalArgumentException("key error错误");
+        throw new IllegalArgumentException("key error错误 !!! 请重新输入key_");
     }
 
 
 
 
     //to catch the error and process ,together to deal with it.
+    //Spring MVC 外的exception或者spring MVC没有处理的Exception，
+    // 就用这种方法进行异常的捕捉。也称为统一处理异常。
     @ExceptionHandler
     @ResponseBody
     public String error(Exception e)
     {
         //Map<String,Object> a=new HashMap<>();
-        return "error"+e.getMessage();
+        return "error message is : "+e.getMessage();
     }
 
 
